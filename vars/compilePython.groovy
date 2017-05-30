c77220c2ceb7145e45aa44c56d38126483d8d7de
Julien Pivotto <roidelapluie@inuits.eu>
2017-05-30 16:04:04 +0200

    add virtualenv support

--- vars/compilePython.groovy
+++ vars/compilePython.groovy
@@ -1,4 +1,8 @@
-def call() {
-    sh """find . -name '*.py' -print0|
-    xargs -0 -t -L 1 python2 -m py_compile"""
+def call(){
+ if (fileExists("requirements.txt")){
+ sh "virtualenv venv"
+ sh "venv/bin/pip install -r requirements.txt"
+ }
+ sh """find . -name '*.py' -print0|
+ xargs -0 -t -L 1 python2 -m py_compile"""
 }
