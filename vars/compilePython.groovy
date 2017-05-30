ccff64f63fa772be360295075dc4fc7296285e32
Julien Pivotto <roidelapluie@inuits.eu>
2017-05-30 16:22:06 +0200

    add PythonCompiler

--- vars/compilePython.groovy
+++ vars/compilePython.groovy
@@ -1,8 +1,7 @@
-def call(String directory = '.'){
- if (fileExists("requirements.txt")){
- sh "virtualenv venv"
- sh "venv/bin/pip install -r requirements.txt"
- }
- sh """find ${directory} -name '*.py' -print0|
- xargs -0 -t -L 1 python2 -m py_compile"""
+import static eu.inuits.PythonCompiler.*
+
+def call(String directory = '.') {
+    echo("Compiling ${directory}")
+    compileDirectory(this, directory)
 }
+
